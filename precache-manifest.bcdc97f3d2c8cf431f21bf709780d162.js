62c0c022afdee5cc636147ca402e555228f07c44
unknown <snuff12@snu.ac.kr>
2020-05-30 17:10:45 +0900

    Updates

--- precache-manifest.bcdc97f3d2c8cf431f21bf709780d162.js
+++ precache-manifest.bcdc97f3d2c8cf431f21bf709780d162.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "36031a6da3749ca061c3bf4815280144",
+    "revision": "dc182691acca5da87b96779094bd8169",
     "url": "/liwoomap/index.html"
   },
   {
-    "revision": "65e013fb2bdb74c7479c",
+    "revision": "f7d83499ab7872127879",
     "url": "/liwoomap/static/css/main.3a6c4815.chunk.css"
   },
   {
-    "revision": "24332792169c33d2beac",
-    "url": "/liwoomap/static/js/2.6f2c5f80.chunk.js"
+    "revision": "e5d1ab3f59d1f2c54270",
+    "url": "/liwoomap/static/js/2.766e5759.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/liwoomap/static/js/2.6f2c5f80.chunk.js.LICENSE.txt"
+    "url": "/liwoomap/static/js/2.766e5759.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "65e013fb2bdb74c7479c",
-    "url": "/liwoomap/static/js/main.00b76b2d.chunk.js"
+    "revision": "f7d83499ab7872127879",
+    "url": "/liwoomap/static/js/main.0a6a9747.chunk.js"
   },
   {
     "revision": "f56a688edb23270fa743",
